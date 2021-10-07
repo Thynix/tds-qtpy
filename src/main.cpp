cb1b7cc8738c2ca7c013d8dba0193a66ef80386e
Steve Dougherty <steve@asksteved.com>
2021-10-07 17:51:46 -0400

    Display on OLED

--- src/main.cpp
+++ src/main.cpp
@@ -24,28 +24,89 @@
 
 #include <Arduino.h>
 #include "GravityTDS.h"
+#include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
-#define TdsSensorPin A1
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+
+// create a pixel strand with 1 pixel on PIN_NEOPIXEL
+Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
+
 GravityTDS gravityTds;
 
-float temperature = 25, tdsValue = 0;
+const size_t sampleCount = 40;
+const size_t sampleMS = 10;
+
+float temperature = 20;
+
+int cmp_float(const void *, const void *);
 
 void setup()
 {
-    Serial.begin(115200);
-    gravityTds.setPin(TdsSensorPin);
-    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
-    gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-    gravityTds.begin();  //initialization
+    Serial.begin(9600);
+    pixels.begin();
+
+    gravityTds.setPin(A10);
+    gravityTds.setAref(3.3);
+    gravityTds.setAdcRange(1 << 12);
+    gravityTds.begin();
+    //temperature = readTemperature();  //add your temperature sensor and read it
+    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+    display.setTextColor(SSD1306_WHITE);
 }
 
 void loop()
 {
-    //temperature = readTemperature();  //add your temperature sensor and read it
-    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
-    gravityTds.update();  //sample and calculate 
-    tdsValue = gravityTds.getTdsValue();  // then get the value
-    Serial.print(tdsValue,0);
-    Serial.println("ppm");
-    delay(1000);
+    gravityTds.update();
+
+    float tdsSamples[sampleCount];
+    for (int i = 0; i < sampleCount; i++) {
+        tdsSamples[i] = gravityTds.getTdsValue();
+        delay(sampleMS);
+    }
+
+    // Use median.
+    qsort(tdsSamples, sampleCount, sizeof(float), cmp_float);
+    float tdsValue = tdsSamples[sampleCount / 2];
+
+    if (tdsValue < 1.0) {
+        // Very low: green
+        pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+    } else if (tdsValue < 6.0) {
+        // Below replace threshold: yellow
+        pixels.setPixelColor(0, pixels.Color(128, 128, 0));
+    } else {
+        // High: red
+        pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+    }
+
+    pixels.show();
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(4);
+    display.print(tdsValue, 0);
+
+    display.setCursor(80, 0);
+    display.setTextSize(2);
+    display.print(" ppm");
+    display.display();
+
+    Serial.print(tdsValue, 0);
+    Serial.println(" ppm");
+}
+
+int cmp_float(const void *a, const void *b)
+{
+    const float *A = (float*) a;
+    const float *B = (float*) b;
+    if (*A < *B) return -1;
+    if (*A > *B) return 1;
+    return 0;
 }
